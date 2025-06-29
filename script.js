000fce2abd2fcfa5a8448f85d6d27373eeb5f8bb
Aniket Tikudave <anikettikudave187@gmail.com>
2025-06-29 15:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  let table = document.getElementById('sampleTable');
+
+  const row=table.insertRow(0);
+
+	const cell1=row.insertCell(0);
+	const cell2=row.insertCell(1);
+
+	cell1.innerText="New Cell1";
+	cell2.innerText="New Cell2";
 }
